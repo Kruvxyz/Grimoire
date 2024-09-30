5f7270d12d1a7b06864518d187229a18af6de4ee
Kruvxyz <golaiet@gmail.com>
2024-09-30 13:26:40 +0300

    add usefull middleware to express

--- BackEnd/Express/Middleware/index.js
+++ BackEnd/Express/Middleware/index.js
@@ -1,4 +1,9 @@
-const express = require('express');
+// const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser'
+import morgan from 'morgan';
+
+
 const app = express();
 const PORT = 3000;
 
@@ -14,14 +19,25 @@ app.use(function (req, res, next) {
     }
     next();
 });
+// Add poluare middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan('dev'));
 
 // Requests will reach this route after 
 // passing through the middleware
 app.get('/user', function (req, res) {
     console.log("/user request called");
-    res.send('Welcome to GeeksforGeeks');
+    res.send('Welcome to X');
 });
 
+// request with body-parser
+app.post('/body', function (req, res) {
+    console.log("/body POST request called");
+    console.log(req.body);
+    res.send('Welcome to X');
+});
+
+// Start the server
 app.listen(PORT, function (err) {
     if (err) console.log(err);
     console.log(`Server listening on PORT ${PORT}`);
